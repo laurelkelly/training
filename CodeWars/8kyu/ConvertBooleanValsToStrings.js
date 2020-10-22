966b35cdc5ccb4ed229c13c38f08bc3280e1c36b
Laurel Kelly <laurellkelly@gmail.com>
2020-10-22 10:20:08 -0700

    Update ConvertBooleanValsToString.js

--- CodeWars/8kyu/ConvertBooleanValsToStrings.js
+++ CodeWars/8kyu/ConvertBooleanValsToStrings.js
@@ -1,6 +1,5 @@
-// 10/21/2020 - Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
-
-// My solution
+// 10/21/2020
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 // input: boolean
 // output: string
